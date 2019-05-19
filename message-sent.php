8e4a43d0914116caf572097a008c4a7dfed7ff98
thearturord <47672781+thearturord@users.noreply.github.com>
2019-05-20 01:34:12 +0200

    a

---
+++ message-sent.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <link rel="shortcut icon" href="img/logo2.png">
+  <meta charset="utf-8">
+  <title>ForexMoneyGrow</title>
+  <meta name="description" content="Forex, Money, Investment.. all the steps to be financially free" />
+  <meta name="author" content="Arturo Nunez" />
+  <meta name="keywords" content="News, Forex, Cryto currency" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/universal-style.css">
+  <link rel="stylesheet" href="css/sent.css">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"> </script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ['Any Google Font']
+      }
+    });
+  </script>
+
+
+</head> <!-- end of head --->
+
+<body>
+
+<?php include("php-template/header-universal.php") ?>
+
+  <div class="contenedordetodo">
+
+    <section class="section">
+
+      <div class="articulos">
+        <!------>
+
+        <article class="main">
+
+          <header>
+            <h1>Privacy Policy</h1>
+          </header>
+
+          <div class="privacy-content">
+
+          <h1>Your Message was successfully sent</h1>
+
+          </div>
+
+        </article>
+
+      </div>
+
+  <!-- <?php include("php-template/aside.php") ?> -->
+    </section>
+
+
+  </div>
+
+  <!-- <div class="home-no">
+
+    <div class="home-b">
+      <a href="index.php">1</a>
+    </div>
+    <div class="home-b">
+      <a href="home2.php">2</a>
+    </div>
+
+  </div> -->
+
+<?php include("php-template/footer.php") ?>
+
+  <script type="text/javascript" src="js/javascript.js"></script>
+
+</body>
+
+</html>
