0680774fbe75c055877a13d8d5651fb317c12a78
thearturord <47672781+thearturord@users.noreply.github.com>
2019-05-22 23:56:57 +0200

    a

--- home-3.php
+++ home-3.php
@@ -133,17 +133,7 @@
 
   </div>
 
-  <div class="home-no">
-    <div class="home-b">
-      <a href="index">1</a>
-    </div>
-    <div class="home-b">
-      <a href="home-2">2</a>
-    </div>
-    <div class="home-b">
-      <a href="home-3">3</a>
-    </div>
-  </div>
+<?php include("php-template/botones.php") ?>
 
 <?php include("php-template/footer.php") ?>
 
