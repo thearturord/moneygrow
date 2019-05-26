4088e03c8e9e1ad264ab4674ff56daf5fa396b36
thearturord <47672781+thearturord@users.noreply.github.com>
2019-05-26 02:37:14 +0200

    Create post-14.php

---
+++ post-14.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <link rel="shortcut icon" href="img/logo2.png">
+  <meta charset="utf-8">
+  <title>ForexMoneyGrow</title>
+  <meta name="description" content="Forex, Money, Investment.. all the steps to be financially free" />
+  <meta name="author" content="Arturo Nunez" />
+  <meta name="keywords" content="News, Forex, Cryto currency" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/universal-style.css">
+  <link rel="stylesheet" href="css/post.css">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+</head> <!-- end of head --->
+
+<body>
+
+<?php include("php-template/header-universal.php") ?>
+
+  <div class="contenedordetodo">
+
+    <section class="section">
+
+      <div class="articulos">
+        <!------>
+
+        <article class="main">
+
+          <h2 class="h2">How does the stock market works?</h2>  <br><br>
+
+          <div class="logo-wrap">
+            <img class="logo-test" src="img/post14.jpg" alt="">
+          </div>
+
+    <p>We all have a more or less close idea of ​​what the Stock Exchange is. We can not define it accurately, but several images come to our mind as soon as we hear about this concept. And you, could you tell us what exactly? Do you have any idea how the Stock Exchange works?</p>
+
+    <h3>Stock stock, some general considerations</h3>
+
+    <p>The Stock Exchange, remember, is that space in which shares of companies are bought and sold. It does not exist as such; that is to say, it is not a physical place as they are, for example, the headquarters of companies or financial corporations.</p>
+
+    <p>This does not mean, however, that it has no impact on organizations or physical entities. On the contrary, many of the movements that take place in the Stock Exchange determine the present and future performance of all the companies that participate actively in it.</p>
+
+    <p>In fact, when it comes to facilitating the explanation of this concept, many experts use the simile of the fruit market in which we can find bananas, strawberries, apples or pears. The difference is that in the Stock Exchange there are no such foods, but rather financial products such as bonds, currencies, credits, debts and derivatives.</p>
+
+    <h3>How the Stock Exchange works: key points</h3>
+
+    <p>To further deepen the operation of the Stock Exchange, let's review a series of concepts and essential points. Let's see:</p>
+
+    <ol>
+
+    <li><b>The Stock Exchange is a generic concept.</b> Actually, there are several exchanges in the world in which companies and entities belonging to specific markets are listed: the Commodity Exchange, the Futures Exchange and the Stock Exchange, among others . All these exchanges can operate in the national, regional or international framework depending on the line of the stock products offered in the market or even market rates.</li>
+    <li><b>The Stock Exchange is a space where</b> companies sell or buy shares. The price of the same is fixed by the free market; that is, those who are willing to buy make an offer and those who want to sell decide whether to accept it or reject it. These are the so-called stock operations.</li>
+    <li><b>Brokers are intermediaries between buyers and sellers of shares.</b> Based on information technology and resources of the digital age, many of them offer investors the purchase of shares in different markets; it is enough for clients to indicate which markets they wish to operate in and brokers take care of the rest. Some brokers have more prestige than companies.</li>
+    <li><b>The offers circulating in the Stock Exchange are publics.</b> For the offer to gain in credibility and prestige, it is normal for those who put shares or stock products on sale to publish all the information related to that offer. From that moment, any company or private agent can know the characteristics of the offer and the financial statements of the companies that have placed it on the Stock Exchange.</li>
+    <li><b>In the Stock Exchange it is important that</b> the information is permanently updated. The objective is for prospective investors to analyze whether the products on offer are suitable for their interests or not. The quality or viability of the offers can vary considerably from one day to the next.</li>
+
+    </ol>
+
+    <p>The Stock Exchange supposes enormous possibilities of increasing (or recovering) the profits of the companies. The key is to make the offer attractive and attract the attention of a good number of investors.</p>
+
+        </article>
+
+      </div>
+
+<?php include("php-template/aside.php") ?>
+    </section>
+
+
+  </div>
+
+<?php include("php-template/footer.php") ?>
+
+  <script type="text/javascript" src="js/javascript.js"></script>
+
+</body>
+
+</html>
