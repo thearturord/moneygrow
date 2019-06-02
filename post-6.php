7180f5f525a38f83855af8babcb707b9d2db1e28
thearturord <47672781+thearturord@users.noreply.github.com>
2019-06-02 23:14:26 +0200

    a

--- post-6.php
+++ post-6.php
@@ -18,11 +18,11 @@
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134739791-3"></script>
   <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  gtag('config', 'UA-134739791-3', { 'optimize_id': 'GTM-KVDZ75V'} );
+    gtag('config', 'UA-134739791-3');
   </script>
 
 </head> <!-- end of head --->
