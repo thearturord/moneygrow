f9d14dadc9ddc55e417ca005f8a54d1b3e5a0049
thearturord <47672781+thearturord@users.noreply.github.com>
2019-06-05 21:16:11 +0200

    a

--- php-template/aside.php
+++ php-template/aside.php
@@ -1,4 +1,6 @@
 <aside class="columna">
+<div class="fixed-columna">
+
 
   <div class="titulodecolumna-wrapper">
     <div class="titulo-caja">
@@ -8,40 +10,51 @@
 
   <div class="lista">
     <ul>
-      <a href="post-1">
+      <a class="li-wrap" href="post-1">
+        <img class="li-img" src="img/post1.jpg" alt="">
         <li>What is forex?</li>
       </a>
-      <a href="post-2">
+      <a class="li-wrap" href="post-2">
+        <img class="li-img" src="img/post2.jpg" alt="">
         <li>What is trading and how do you earn money by investing?</li>
       </a>
-      <a href="post-3">
+      <a class="li-wrap" href="post-3">
+        <img class="li-img" src="img/post3.jpg" alt="">
         <li>Risk Control</li>
       </a>
-      <a href="post-4">
+      <a class="li-wrap" href="post-4">
+        <img class="li-img" src="img/post4.jpg" alt="">
         <li>Financial Education</li>
       </a>
-      <a href="post-5">
+      <a class="li-wrap" href="post-5">
+        <img class="li-img" src="img/post5.jpg" alt="">
         <li>4 of the most successful forex traders in the world</li>
       </a>
-      <a href="post-6">
+      <a class="li-wrap" href="post-6">
+        <img class="li-img" src="img/post6.jpg" alt="">
         <li>School of business</li>
       </a>
-      <a href="post-7">
+      <a class="li-wrap" href="post-7">
+        <img class="li-img" src="img/post7.jpg" alt="">
         <li>The idea behind the Forex strategy</li>
       </a>
-      <a href="post-8">
+      <a class="li-wrap" href="post-8">
+        <img class="li-img" src="img/post8.jpg" alt="">
         <li>Security in your transactions</li>
       </a>
-      <a href="post-9">
+      <a class="li-wrap" href="post-9">
+        <img class="li-img" src="img/post9.jpg" alt="">
         <li>10 things you should know about the Forex market</li>
       </a>
-      <a href="post-10">
+      <a class="li-wrap" href="post-10">
+        <img class="li-img" src="img/post10.jpg" alt="">
         <li>What Forex Brokers Never Want you to Know!</li>
       </a>
-      <a href="post-11">
+      <a class="li-wrap" href="post-11">
+        <img class="li-img" src="img/post11.jpg" alt="">
         <li>Beginner's Guide to Forex Trading</li>
       </a>
     </ul>
   </div>
-
+</div>
 </aside>
