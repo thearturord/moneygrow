c4e0661d4dbd6fa6492c9d0e1a232fbfe7ec2b75
thearturord <47672781+thearturord@users.noreply.github.com>
2019-06-15 18:37:46 +0200

    Update post-11.php

--- post-11.php
+++ post-11.php
@@ -161,7 +161,7 @@
             <p>The main Forex pairs tend to be the most liquid and, therefore, provide the most opportunities for short-term trading. However, there are also many opportunities between minor and exotic currencies, especially if you have some specialized knowledge about a certain currency.</p>
 
             <div style="padding-bottom:56.25%; position:relative; display:block; width: 100%">
-             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/9LqET1ZPFeU" frameborder="0" allowfullscreen=""
+             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/cxeqArWwmVE" frameborder="0" allowfullscreen=""
               style="position:absolute; top:0; left: 0"></iframe>
             </div>
 
