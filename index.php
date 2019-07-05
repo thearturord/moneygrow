a2b37373723c8868aa94cf8f7ca2c7b9ca5bce3f
thearturord <47672781+thearturord@users.noreply.github.com>
2019-07-05 17:18:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,14 @@
     gtag('config', 'UA-134739791-3');
   </script>
 
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3177332832089381",
+    enable_page_level_ads: true
+  });
+</script>
+
 </head> <!-- end of head --->
 
 <body>
