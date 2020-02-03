6e4e0761791de57d0d00dc4a3e6212fe3a486316
thearturord <47672781+thearturord@users.noreply.github.com>
2020-02-03 19:06:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -53,24 +53,6 @@
 
     <section class="section">
 
-      <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '176296587022787',
-      xfbml      : true,
-      version    : 'v6.0'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 
       <div class="articulos">
         <!------>
@@ -205,6 +187,25 @@
 <?php include("php-template/footer.php") ?>
 
   <script type="text/javascript" src="js/javascript.js"></script>
+  <script>
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '176296587022787',
+  xfbml      : true,
+  version    : 'v6.0'
+});
+FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "https://connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
 
 </body>
 
