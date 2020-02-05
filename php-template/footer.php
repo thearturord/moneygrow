33742e69567440230f5c277055a7b326a62d9253
thearturord <47672781+thearturord@users.noreply.github.com>
2020-02-05 03:03:29 +0100

    Update footer.php

--- php-template/footer.php
+++ php-template/footer.php
@@ -5,6 +5,27 @@
   data-show-faces="true">
 </div>
 
+<script>
+window.fbAsyncInit = function() {
+FB.init({
+appId      : '176296587022787',
+xfbml      : true,
+version    : 'v6.0'
+});
+FB.AppEvents.logPageView();
+});
+
+};
+
+(function(d, s, id){
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) {return;}
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
 <footer class="pie">
   Copyright © 2019 ForexMoneyGrow. All rights reserved
 </footer>
