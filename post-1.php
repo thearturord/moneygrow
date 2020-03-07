dbbeeffe547339188ea222b7885e18fc3ac15dc0
thearturord <47672781+thearturord@users.noreply.github.com>
2020-03-07 15:07:18 +0100

    Update post-1.php

--- post-1.php
+++ post-1.php
@@ -52,21 +52,30 @@
         });
 
         FB.AppEvents.logPageView();
-        //
-        // FB.getLoginStatus(function(response) {
-        //   statusChangeCallback(response);
-        //   console.log(response);
-        // });
+
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
 
       };
 
       (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
+
          if (d.getElementById(id)) {return;}
          js = d.createElement(s); js.id = id;
          js.src = "https://connect.facebook.net/en_US/sdk.js";
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
+
+       function statusChangeCallback(response){
+         if (response.status === "connected") {
+           console.log("you are in");
+         } else{
+           console.log("you are out");
+         }
+       }
+
     </script>
 
 
