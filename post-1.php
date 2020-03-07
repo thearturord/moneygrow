4159b3986381a9fe0b103ccada5191193182ff5d
thearturord <47672781+thearturord@users.noreply.github.com>
2020-03-07 13:41:24 +0100

    a

--- post-1.php
+++ post-1.php
@@ -42,14 +42,14 @@
     <article>
       <header>
         <!-- The title and subtitle shown in your Instant Article -->
-        <h1>Article Title</h1>
+        <h1 class="h2">What is Forex?</h1><br><br>
         <h2>Article Subtitle</h2>
 
         <!-- The date and time when your article was originally published -->
-        <time class="op-published" datetime="2019-11-11T04:44:16Z">November 11th, 4:44 PM</time>
+        <time class="op-published" dateTime="2020-2-03T04:44:16Z">February 3rd, 4:44 PM</time>
 
         <!-- The date and time when your article was last updated -->
-        <time class="op-modified" dateTime="2019-12-11T04:44:16Z">December 11th, 4:44 PM</time>
+        <time class="op-modified" dateTime="2020-2-03T04:45:18Z">February 3rd, 4:45 PM</time>
 
         <!-- The authors of your article -->
         <address>
@@ -78,7 +78,75 @@
       <!-- Article body goes here -->
 
       <!-- Body text for your article -->
-      <p> Article content </p>
+      <p>
+        The Forex exchange market is the abbreviation in English of Foreign Exchange Currency Market, that is to say: Exchange Market of Currencies (currencies). Where the trader seeks profits through fluctuations in the exchange rate. </p>
+
+      <p>
+        A currency is earned at the top (when bought) and can also be earned when a currency falls (when sold). However, the opposite may occur, that is to say that we are in a position of purchase for a certain currency and this currency instead of rising (its price) begins to fall; which would affect our account. The same (in the opposite direction) would happen for sales. Therefore it is not as easy as it may seem. The Foreign Exchange Market offers extraordinary profit opportunities, however, keep in mind that it is also a tool of considerable risk to invest capital that is part of your savings surplus.
+      </p>
+
+      <h3>What kind of operations do they carry out?</h3>
+
+      <p>
+        Basically buying and selling currencies. Which are invested in pairs such as EUR / USD (Euro, Dollar) GBP / USD (Pound, Dollar), etc. These are the pairs that you will see in the operations in this blog, but there are up to about 30 pairs
+        of different currencies. With which you can generate an attractive investment portfolio.
+      </p>
+
+      <h3>Since when does FOREX exist and what is its size?</h3>
+
+      <p>
+        The currency market was created in 1977 and the Central Reserve Banks and large investment banks participated. Nowadays, any small investor can participate through a business house or Broker and from his comfort of his domicile he can
+        download a demo platform where he can practice with virtual money. These platforms are free.
+      </p>
+      <p>
+        In terms of volumes, the size of the Foreign Exchange Market is much larger than all the combined Stock Exchanges of the world. It is also the most liquid market, where in 2009 it exceeded the amount of $ 3 trillion dollars per day, so
+        its liquidity is impressive, facilitating intraday operations in 1 minute charts.
+      </p>
+
+      <h3>What advantages does an investor in the FOREX market have?</h3>
+
+      <p>
+        You can win when a currency depreciates (when its value falls) or when it appreciates (by increasing its value). It means that you can take advantage (for example) when the dollar increases its value (we buy dollars) and when this
+        currency falls (we sell dollars). This is a great advantage because unlike the Argentine Market in this one can only make purchase operations and not sales operations (in short) and then buy when the value falls.</p>
+
+      <p>
+        The currencies show identifiable trends. From them, projections and strategies of buying and selling currency are made in the short, medium and long term.</p>
+
+      <p>
+        There are about 30 crosses of currencies available, therefore you can diversify the investment portfolio and not depending on a single currency. You can have investments in Dollars, Euros, Yen, Pounds, Swiss Francs, etc. </p>
+
+      <p>
+        It is an extremely liquid market that works 24 hours a day, so you can withdraw or place more capital at any time, associated with your type of strategy.
+      </p>
+
+      <h3>What is the risk and what disadvantage does it have?</h3>
+
+      <p>
+        A palpable disadvantage is its extreme volatility. Which can take us out of positions at any time of the market causing losses. Therefore, your broker must provide you with a tracking platform in order for you to monitor the
+        operations of your account in real time.</p>
+
+      <p>
+        There are "companies" on the internet that offer extraordinary returns of 30% and up to 50% per month. These are no more than scammers and people who seek to take advantage of the little training and knowledge of those who have just taken
+        their first steps in FOREX. Be very careful with extraordinary offers of profits.
+      </p>
+
+      <h3>Can I lose my investment?</h3>
+
+      <p>
+        It is possible that you may have some loss, however you can control your risk with the "Stop Loss" and only risk your capital to a certain level (you monitor the operations from the Internet) depending on your operating style.
+      </p>
+
+      <h3>How do I participate in the FOREX Market?</h3>
+
+      <p>
+        You can participate with any business house or broker in the world. The largest and most respected companies are usually from the USA and Europe and must be fully regulated. When choosing one, consider the age of the broker and make sure
+        it is supervised by the regulators of that country (NFA, FSA, CFTC).</p>
+
+      <p>
+        Another very important point, is that it is not recommended to enter the FOREX market by intermediaries who manage their money, it is always advisable that you are the ones who invest and in this way know the risk to which they are
+        exposed, do not forget that it is a very dangerous market , where are the best investors in the world. If you do not know how to operate, then it is better to stay on the sidelines until you have enough experience in the different Demo
+        accounts offered by the Brokers.
+      </p>
 
       <!-- A video within your article -->
       <!-- TODO: Change the URL to a live video from your website -->
